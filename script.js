350362cbab170f7a0fdeca9aeba5047c2ed33a4a
skif-cezar <skif.cezar@gmail.com>
2020-03-21 15:03:05 +0300

    Header. Smooth menu navigation

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 
+/* header */
+
+const menu = document.querySelector("nav ul");
+const menuLinks = document.querySelectorAll("nav ul li a");
+
+menuLinks.forEach( link => link.addEventListener("click", (event) => {
+    menu.querySelectorAll('a').forEach(e => e.classList.remove('current'));
+event.target.classList.add("current");
+}));
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        document.getElementById(blockID).scrollTop += 10;
+    })
+
+};
+
 /* vertical and horizontal iPhone */
+
 const phoneVertical = document.getElementById('phone__vertical');
 const screen1 = document.getElementById('screen-1');
 const phoneHorizontal = document.getElementById('phone__horizontal');
