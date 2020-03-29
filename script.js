8c42af1d4c5dfeb52842339706f9c54b78eaf4c8
skif-cezar <skif.cezar@gmail.com>
2020-03-29 22:54:19 +0300

    Responsive. Header, Slider, Services

--- script.js
+++ script.js
@@ -83,6 +83,66 @@ phoneHorizontal.addEventListener('click', function(){
     clickAll(screen2);
 });
 
+/* slider */
+
+let items = document.querySelectorAll(".item");
+let currentItem = 0;
+let isEnabled = true;
+let slider = document.getElementsByClassName("slider");
+
+function changeCurrentItem(n) {
+    currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener('animationend', function () {
+        this.classList.remove('active', direction);
+    });
+}
+
+function showItem(direction) {
+    items[currentItem].classList.add('next', direction);
+    items[currentItem].addEventListener('animationend', function () {
+        this.classList.remove('next', direction);
+        this.classList.add('active');
+        isEnabled = true;
+    });
+}
+
+function previousItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n - 1);
+    showItem('from-left');
+    if (slider[0].classList[1] == "blue") {
+        slider[0].classList.remove('blue');
+    }
+    else { slider[0].classList.add('blue'); }
+}
+
+function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n + 1);
+    showItem('from-right');
+    if (slider[0].classList[1] == "blue") {
+        slider[0].classList.remove('blue');
+    }
+    else { slider[0].classList.add('blue'); }
+}
+
+document.querySelector('.control.left').addEventListener('click', function () {
+    if (isEnabled) {
+        previousItem(currentItem);
+    }
+});
+
+document.querySelector('.control.right').addEventListener('click', function () {
+    if (isEnabled) {
+        nextItem(currentItem);
+    }
+});
+
 /* portfolio */
 
 const tags = document.querySelectorAll(".container__buttons button");
